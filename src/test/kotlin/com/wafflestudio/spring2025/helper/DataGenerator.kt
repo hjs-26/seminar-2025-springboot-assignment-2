37ec40f279057f2db1227068e65acb5303d5c5e6
yeonseo432 <yeonseo432@snu.ac.kr>
2025-11-04 04:36:14 +0900

    feat: course 검색 및 단건 조회 test 구현

--- src/test/kotlin/com/wafflestudio/spring2025/helper/DataGenerator.kt
+++ src/test/kotlin/com/wafflestudio/spring2025/helper/DataGenerator.kt
@@ -5,6 +5,10 @@ import com.wafflestudio.spring2025.board.repository.BoardRepository
 import com.wafflestudio.spring2025.comment.model.Comment
 import com.wafflestudio.spring2025.comment.repository.CommentRepository
 import com.wafflestudio.spring2025.common.enum.Semester
+import com.wafflestudio.spring2025.course.crawling.ClassPlaceAndTime
+import com.wafflestudio.spring2025.course.crawling.DayOfWeek
+import com.wafflestudio.spring2025.course.model.Course
+import com.wafflestudio.spring2025.course.repository.CourseRepository
 import com.wafflestudio.spring2025.post.model.Post
 import com.wafflestudio.spring2025.post.repository.PostRepository
 import com.wafflestudio.spring2025.timetable.model.Timetable
@@ -23,6 +27,7 @@ class DataGenerator(
     private val postRepository: PostRepository,
     private val commentRepository: CommentRepository,
     private val timetableRepository: TimetableRepository,
+    private val courseRepository: CourseRepository,
     private val jwtTokenProvider: JwtTokenProvider,
 ) {
     fun generateUser(
@@ -99,5 +104,50 @@ class DataGenerator(
                 ),
             )
         return timetable
+    }
+
+    fun generateCourse(
+        year: Int? = null,
+        semester: Semester? = null,
+        classification: String? = null,
+        college: String? = null,
+        department: String? = null,
+        academicCourse: String? = null,
+        academicYear: String? = null,
+        courseNumber: String? = null,
+        lectureNumber: String? = null,
+        courseTitle: String? = null,
+        credit: Long? = null,
+        instructor: String? = null,
+        classTimeJson: List<ClassPlaceAndTime>? = null,
+    ): Course {
+        val course =
+            courseRepository.save(
+                Course(
+                    year = year ?: 2025,
+                    semester = semester ?: Semester.FALL,
+                    classification = classification ?: "전공필수",
+                    college = college ?: "공과대학",
+                    department = department ?: "컴퓨터공학부",
+                    academicCourse = academicCourse ?: "학사",
+                    academicYear = academicYear ?: "1학년",
+                    courseNumber = courseNumber ?: "M1522.${Random.Default.nextInt(100000, 999999)}",
+                    lectureNumber = lectureNumber ?: "00${Random.Default.nextInt(1, 9)}",
+                    courseTitle = courseTitle ?: "강의-${Random.Default.nextInt(1000000)}",
+                    credit = credit ?: 3L,
+                    instructor = instructor ?: "교수-${Random.Default.nextInt(1000)}",
+                    classTimeJson =
+                        classTimeJson
+                            ?: listOf(
+                                ClassPlaceAndTime(
+                                    day = DayOfWeek.MONDAY,
+                                    place = "301-101",
+                                    startMinute = 540,
+                                    endMinute = 630,
+                                ),
+                            ),
+                ),
+            )
+        return course
     }
 }
