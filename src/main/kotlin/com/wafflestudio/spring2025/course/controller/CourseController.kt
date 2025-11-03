6a4f050311b6d14f981a9c9e42c8659a85032a34
yeonseo432 <yeonseo432@snu.ac.kr>
2025-11-04 02:31:48 +0900

    fix: GetMapping에서 @RequestBody 사용하던 것을 @RequesParam으로 변경

--- src/main/kotlin/com/wafflestudio/spring2025/course/controller/CourseController.kt
+++ src/main/kotlin/com/wafflestudio/spring2025/course/controller/CourseController.kt
@@ -1,5 +1,6 @@
 package com.wafflestudio.spring2025.course.controller
 
+import com.wafflestudio.spring2025.common.enum.Semester
 import com.wafflestudio.spring2025.course.dto.CourseSearchRequest
 import com.wafflestudio.spring2025.course.dto.CourseSearchResponse
 import com.wafflestudio.spring2025.course.service.CourseService
@@ -7,25 +8,32 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/courses")
+@RequestMapping("/api/v1/course")
 class CourseController(
     private val courseService: CourseService,
 ) {
     @GetMapping
     fun search(
-        @RequestBody searchRequest: CourseSearchRequest,
+        @RequestParam year: Int,
+        @RequestParam semester: Semester,
+        @RequestParam(required = false) keyword: String?,
+        @RequestParam(required = false) nextId: Long?,
+        @RequestParam(defaultValue = "20") limit: Int,
     ): ResponseEntity<CourseSearchResponse> {
         val coursePagingResponse =
             courseService.searchByYearSemesterKeyword(
-                year = searchRequest.year,
-                semester = searchRequest.semester,
-                keyword = searchRequest.keyword,
-                nextId = searchRequest.nextId,
-                limit = searchRequest.limit,
+                year = year,
+                semester = semester,
+                keyword = keyword,
+                nextId = nextId,
+                limit = limit,
             )
         return ResponseEntity.ok(coursePagingResponse)
     }
+
+
 }
