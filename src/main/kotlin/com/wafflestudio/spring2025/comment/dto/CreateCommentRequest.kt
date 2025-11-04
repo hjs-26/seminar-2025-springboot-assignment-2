c97314025502e4ef2095dc5cc22963a27afa04ee
yeonseo432 <yeonseo432@snu.ac.kr>
2025-11-04 21:21:28 +0900

    dto에 swagger 어노테이션 추가

--- src/main/kotlin/com/wafflestudio/spring2025/comment/dto/CreateCommentRequest.kt
+++ src/main/kotlin/com/wafflestudio/spring2025/comment/dto/CreateCommentRequest.kt
@@ -1,5 +1,9 @@
 package com.wafflestudio.spring2025.comment.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "댓글 작성 요청")
 data class CreateCommentRequest(
+    @Schema(description = "댓글 내용", example = "좋은 게시글이네요!", required = true)
     val content: String,
 )
--- src/main/kotlin/com/wafflestudio/spring2025/comment/dto/UpdateCommentRequest.kt
+++ src/main/kotlin/com/wafflestudio/spring2025/comment/dto/UpdateCommentRequest.kt
@@ -1,5 +1,9 @@
 package com.wafflestudio.spring2025.comment.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "댓글 수정 요청")
 data class UpdateCommentRequest(
+    @Schema(description = "댓글 내용", example = "수정된 댓글입니다.", required = true)
     val content: String,
 )
